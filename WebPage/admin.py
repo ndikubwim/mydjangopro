c25d9f02ab561150e66c1b7ba1bcc8aeb9d092b9
ndikubwim <88678975+ndikubwim@users.noreply.github.com>
2022-08-12 16:56:52 +0200

    Add files via upload

---
+++ WebPage/admin.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from .models import Post,Product,Student,Staff,Posting,profile,Announcement,Question,New,Course,Book,Score,multipleQuestion,DetailQuestion,MathQuestion
+# Register your models here.
+admin.site.register(Post)
+admin.site.register(Product)
+admin.site.register(Student)
+admin.site.register(Staff)
+admin.site.register(Posting)
+admin.site.register(profile)
+admin.site.register(Announcement)
+admin.site.register(Question)
+admin.site.register(New)
+admin.site.register(Course)
+admin.site.register(Book)
+admin.site.register(Score)
+admin.site.register(multipleQuestion)
+admin.site.register(DetailQuestion)
+admin.site.register(MathQuestion)
